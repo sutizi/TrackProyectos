a21a531c404c0924ea3a89b2b96ddd5b405389ce
SebasianUt <sebastianutizi@gmail.com>
2020-01-09 06:04:00 -0300

    Configuracion inicial Angular

---
+++ TrackProyectosFront/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProyectoComponent } from './proyecto/proyecto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProyectoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
